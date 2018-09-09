162fa1c8ddcd7a43d7e0e2fe2bb5c14f9c8fbfc2
Manjur Khan <manjur.khan@stonybrook.edu>
2018-09-09 10:35:30 +0000

    Many changes, once again.

--- pennappsXVIII/server/enbrava-api/index.js
+++ pennappsXVIII/server/enbrava-api/index.js
@@ -4,11 +4,18 @@ const docusign = require("docusign-esign");
 const bodyParser = require("body-parser");
 const DB_NAME = "endrava";
 const ACCOUNT_ID = "6448450";
+const request = require("request");
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 var apiClient = new docusign.ApiClient();
 var BaseUrl = 'https://demo.docusign.net/restapi';
 apiClient.setBasePath(BaseUrl);
-accessToken ="eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwCALDQPDBbWSAgAgGxXHU8W1kgCAMxdo4SiMShNmf0SLQF_PrcVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MAAAQUtaBhbWSA.ytBD9tIMjmIqm3R1wZi75r2IRoLcHuvharf5_2DxjTcYNLkK4KDky3xagfYUB0Qno13leVXfZm-ga3ZepJwBRetIJvGcqT9svFHP5b6ukU0ZbbpZQmQOT_L3LiybRFtjTzZmR4Zj1a1dBKkmr1nBLT7SVljwElTrMHOuoiz4BBnRVVZv3mWCsiWU7xvLnrgqX363WLG6dbNUycgp_39J0_HKHDtOgwzRih6wQ-cF-grqsavMj8MplL8oxRiIFtS5j1lekw_boWs0KMqq40TYTQux0HWZzvXMV93Mjy8m5JdvfNsM3h0kvLAx1PRwsevsHt7HbpOx8w-uVlFdobuLTg";
+accessToken =
+"eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwAATOtVNxbWSAgAAIwOZHoW1kgCAMxdo4SiMShNmf0SLQF_PrcVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MAAAKO4XNxbWSA.OvpIJ6NjPnJGWbxvISiwDK05DeHzB9f1KlCGvbP4Qp4-ziJ-eqJ09ZxHjbvfDP4suhbrdIHVbTmN0p7A0ko3mX0kGzKZFQ4xdiKltvYZDAqV8qJPN31nVoZvHepvSlSvDnyCSTy5hZmn_D8Q4lDWObg2g8xyOK6R3igE4oiQSboAg8EU0M8tAtYTskP3aL0y6gxA6h_qYm-aGlHZfV-p2xUGJkKgg9Mk4XDJFkwfG1SNTU5Uu7ek0GqQNe2Mt6QapZE4XfdUyqAJ_aKv8jjVu4XzKrrdRB0fQHKoXkXk6vfTzih3eLAN3gHfeoEMtoPVvmARA8ishC9SwDDg5vfF2w";
 apiClient.addDefaultHeader("Authorization", "Bearer " + accessToken);
 var client = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/";
@@ -136,4 +143,13 @@ app.get("/envelopes", function(req, res) {
             res.send(data);
         });
     });
+});
+
+app.get("/pdf/:envelopeId", function (req, res) {
+    request({"headers":
+    {"Authorization": "Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwAATOtVNxbWSAgAAIwOZHoW1kgCAMxdo4SiMShNmf0SLQF_PrcVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MAAAKO4XNxbWSA.OvpIJ6NjPnJGWbxvISiwDK05DeHzB9f1KlCGvbP4Qp4-ziJ-eqJ09ZxHjbvfDP4suhbrdIHVbTmN0p7A0ko3mX0kGzKZFQ4xdiKltvYZDAqV8qJPN31nVoZvHepvSlSvDnyCSTy5hZmn_D8Q4lDWObg2g8xyOK6R3igE4oiQSboAg8EU0M8tAtYTskP3aL0y6gxA6h_qYm-aGlHZfV-p2xUGJkKgg9Mk4XDJFkwfG1SNTU5Uu7ek0GqQNe2Mt6QapZE4XfdUyqAJ_aKv8jjVu4XzKrrdRB0fQHKoXkXk6vfTzih3eLAN3gHfeoEMtoPVvmARA8ishC9SwDDg5vfF2w"
+    },
+    uri: "https://demo.docusign.net/restapi/v2/accounts/c77125a6-7d56-4017-bb5c-b1ed9a8819f6/envelopes/"+ req.params.envelopeId +
+    "/documents/combined",
+    method: "GET"}).pipe(res);
 });
